f45d43e2c1c2e7098e20479934a965a4148ca3d8
ethanlikescpp <ethanlikespi@gmail.com>
2017-05-08 21:15:05 -0700

    Working on dynamic memory

--- MoreClassesStuff/MoreClassesStuff.cpp
+++ MoreClassesStuff/MoreClassesStuff.cpp
@@ -7,6 +7,38 @@
 
 using namespace std;
 
+class Prices {
+public:
+	double price, yield; // Variable declarations come first
+	Prices() {
+		cout << "This will also be printed automatically" << endl;
+		price = 0; // Can initialize the variables in here
+		yield = 0;
+	}
+};
+
+class Stock {
+public:
+	Stock() {
+		cout << "This will be run automatically when object is created" << endl;
+		
+	}
+	Prices yesterday, today;
+
+	// Stock -> Double
+	// Returns the stock price prediction of the next day, linearly
+	double prediction() { // This is called a "method"
+		double rate = today.price / yesterday.price;
+		return rate * today.price;
+	}
+
+};
+
+// Put function declarations below the classes!
+// Otherwise you won't be able to declare functions with respective classes in them!
+
+//double prediction(Stock&);
+
 // Void -> Int
 // The main function of this program
 int main() {
@@ -15,5 +47,26 @@ int main() {
 	int exp1 = 2;
 	cout << "power is: " << pow(base1, exp1) << endl;
 
+	cout << endl << "PART 2.2: CONSTUCTORS AND DECONSTRUCTERS" << endl;
+
+	Stock msft;
+	msft.yesterday.price = 101.5;
+	msft.yesterday.yield = 0.02;
+	msft.today.price = 105.3;
+	msft.today.yield = 0.01;
+
+	cout << "Microsoft's price yesterday: " << msft.yesterday.price << endl;
+	cout << "Microsoft's divedend yesterday: " << msft.yesterday.yield << endl;
+	cout << "Microsoft's price today: " << msft.today.price << endl;
+	cout << "Microsoft's divedend today: " << msft.today.yield << endl;
+
+	double tomorrowprice = msft.prediction();
+
+	cout << "Predicted price for tomorrow: " << tomorrowprice << endl;
+
+	Stock *pAmd = new Stock();
+	
+
     return 0;
 }
+
---
+++ MoreClassesStuff/Person.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Person.h"
+#include <iostream>
+
+Person::Person() {
+}
+
+Person::Person(string fName, string lName) {
+	firstName = fName;
+	lastName = lName;
+}
+
+Person::Person(string fName, string lName, int age) {
+	firstName = fName;
+	lastName = lName;
+	age = age;
+}
+
+Person::~Person() {
+}
